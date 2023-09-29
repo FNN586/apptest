ddfb0f55c90d7951e71d8b4c67f84c8835b7b3e1
Fabienne <fablene@yahoo.fr>
2023-09-29 15:58:00 +0200

    ajout nouveau composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NouveauComponent } from './nouveau/nouveau.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NouveauComponent
   ],
   imports: [
     BrowserModule
